7f46165e01e5ea211d71e6fae5d61669fff0332c
velappan <velappan.t@appstarsapplications.in>
2025-05-29 02:27:24 +0530

    Update header.jsx

--- src/layout-component/header.jsx
+++ src/layout-component/header.jsx
@@ -142,21 +142,16 @@ const Header = ({
                   }
                   className='text-black dark:text-white'
                 >
-                    {
-                      resolvedTheme === "light" ?
-                      <div className="menu-items cursor-pointer text-black dark:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-                        </svg>
-                      </div> :
-                      <div className="menu-items cursor-pointer text-black dark:text-white">
-                        <div className="icon">
-                          <svg width="24" height="24" viewBox="0 0 24 24" className="w-6 h-6" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 2V3M12 21V22M22 12H21M3 12H2M19.07 4.93L18.678 5.323M5.322 18.678L4.929 19.071M19.07 19.07L18.678 18.677M5.322 5.322L4.929 4.929M6.341 10C5.88777 11.2831 5.88415 12.6821 6.33073 13.9675C6.77732 15.2529 7.64758 16.3484 8.79871 17.074C9.94984 17.7997 11.3135 18.1125 12.6658 17.9612C14.0181 17.8098 15.2788 17.2033 16.2411 16.2411C17.2033 15.2788 17.8098 14.0181 17.9612 12.6658C18.1125 11.3135 17.7997 9.94984 17.074 8.79871C16.3484 7.64758 15.2529 6.77732 13.9675 6.33073C12.6821 5.88415 11.2831 5.88777 10 6.341" stroke="#fff" strokeWidth="1.5" strokeLinecap="round"/>
-                          </svg>
-                        </div>
-                      </div>
-                    }
+                  <div className="menu-items cursor-pointer dark:hidden text-black dark:text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                    </svg>
+                  </div>
+                  <div className="menu-items cursor-pointer hidden dark:block text-black dark:text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                    </svg>
+                  </div>
                 </button>
                 <div className='hidden md:block'>
                   <Dropdown placement="bottom-start">
